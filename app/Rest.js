bde69ba4ff3077507dd71b5a0fdb3243179bd333
Loic <jeschonek.l@gmail.com>
2020-11-16 15:17:17 +0100

    Step 1

---
+++ app/Rest.js
@@ -0,0 +1,29 @@
+class Rest{
+
+    static get(data){ // table, id, where, order
+        return $.get("rest/", data)
+    }
+
+    static post(data) { //table, fields (k,v)
+        return $.post("rest/", data)
+
+    }
+
+    static put(data){ //table, id, fields (k,v)
+        return $.ajax({
+            url: "rest/",
+            type: 'PUT',
+            data : JSON.stringify(data)       
+
+           })
+        }
+
+    static delete(data){ //table, id
+        return $.ajax({
+            url: "rest/",
+            type: 'DELETE',
+            data : JSON.stringify(data)
+        })
+
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,1 +1,58 @@
 App.start();
+
+
+
+// Tests : Qui sont sensés marcher (et marchent bien)
+
+
+Rest.get({table:"Billy",id:0,where:"Test du where",order:"Test de l'order"}).done((resp) => {
+    console.log(`Résultat de la requête get : ${resp}`)
+})
+
+Rest.post({table : "Billy", test1 : "Blblbl", test2 : "Blblbl 2"}).done((resp) => {console.log(`Résultat de la requête post : ${resp}`)});
+
+
+Rest.put ({
+    table : "Billy",
+    id : 0,
+    repas : "Pates au beurre",
+    boisson : "Eau plate"
+}).done((resp) => {
+    console.log("Résultat de la requête put " + resp);
+})
+
+Rest.delete({
+    table : "Billy",
+    id : 0,
+}).done((resp) => {
+    console.log(`Résultat de la requête Delete : ${resp}`)
+})
+
+
+
+
+// Tests : Qui sont sensés être des erreurs (et sont donc stoppés via un false / break)
+
+Rest.get({
+    tablz : "Non valide"
+}).done((resp) => {
+    console.log("Sans table : requête get = " + resp)
+})
+
+Rest.post({
+    tablz : "Non valide"
+}).done((resp) => {
+    console.log("Sans table : requête post = " + resp)
+})
+
+Rest.put({
+    tablz : "Non valide"
+}).done((resp) => {
+    console.log("Sans table et/ou sans ID: requête put = " + resp)
+})
+Rest.delete({
+    tablz : "Non valide"
+}).done((resp) => {
+    console.log("Sans table et/ou sans ID: requête delete = " + resp)
+
+})
